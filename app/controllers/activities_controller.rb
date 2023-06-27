5edc34d540ddafc3e37f97442cf53d66044f5937
Cyrille Varin <cyrille.varin@live.fr>
2023-06-27 15:29:11 +0200

    indexouille

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,35 @@
 class ActivitiesController < ApplicationController
+  before_action :set_user_address, only: :index
+
   def index
-    @activities = Activity.all
+    if params[:query].present?
+      sql_query = <<~SQL
+        activities.title ILIKE :query
+        OR activities.category ILIKE :query
+        OR activities.content ILIKE :query
+        OR activities.address ILIKE :query
+        OR users.first_name ILIKE :query
+      SQL
+      @activities = Activity.joins(:user).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @activities = Activity.all
+    end
+
+    if user_signed_in? && @user_address.present?
+      user_coordinates = Geocoder.coordinates(@user_address)
+
+      if user_coordinates.present?
+        # Trouver les activités autour de l'utilisateur
+        @activities = @activities.near(user_coordinates, 10, units: :km)
+      end
+    end
+
+    params[:query] = nil
+  end
+
+  private
+
+  def set_user_address
+    @user_address = current_user&.address
   end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,7 @@
 class Activity < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   belongs_to :user
   has_many :reservations
   has_many :likes
