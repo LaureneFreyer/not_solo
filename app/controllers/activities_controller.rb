488aded6a065a23a76cc1f57961f6b83476593b8
Cyrille Varin <cyrille.varin@live.fr>
2023-06-27 21:03:24 +0200

    ok

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -16,15 +16,48 @@ class ActivitiesController < ApplicationController
     end
 
     if user_signed_in? && @user_address.present?
-      user_coordinates = Geocoder.coordinates(@user_address)
+      @user_coordinates = Geocoder.coordinates(@user_address)
 
-      if user_coordinates.present?
+      if @user_coordinates.present?
         # Trouver les activités autour de l'utilisateur
-        @activities = @activities.near(user_coordinates, 10, units: :km)
+        @activities = @activities.near(@user_coordinates, 10, units: :km)
+        @user_coordinates = @user_coordinates.reverse # Inverser l'ordre des coordonnées (latitude, longitude -> longitude, latitude)
       end
     end
 
+
     params[:query] = nil
+
+    @markers = @activities.map do |activity|
+      {
+        lat: activity.latitude,
+        lng: activity.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {activity: activity}),
+        marker_html: render_to_string(partial: "marker")
+      }
+    end
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.user = current_user # suppose que l'utilisateur est connecté
+
+    # Ajoutez ces lignes pour obtenir les coordonnées
+    if @activity.address.present?
+      coordinates = Geocoder.coordinates(@activity.address)
+      @activity.latitude = coordinates.first
+      @activity.longitude = coordinates.last
+    end
+
+    if @activity.save
+      redirect_to activity_path(@activity)
+    else
+      render :new
+    end
   end
 
   private
@@ -32,4 +65,9 @@ class ActivitiesController < ApplicationController
   def set_user_address
     @user_address = current_user&.address
   end
+
+  def activity_params
+    params.require(:activity).permit(:category, :latitude, :longitude, :title, :price_person, :content, :address, :start_date, :end_date)
+  end
+
 end
