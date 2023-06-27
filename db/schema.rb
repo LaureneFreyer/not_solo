75209b68ccd4e8459cb7740a12292e935a6418d7
Laurene Freyer <laurene.lepvrier@hotmail.fr>
2023-06-27 15:28:12 +0200

    home

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_141950) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_27_122121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,6 +93,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_26_141950) do
     t.date "birthday"
     t.text "description"
     t.string "interest"
+    t.string "genre"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
