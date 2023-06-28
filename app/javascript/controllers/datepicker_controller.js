88ff85e116c4fe0989238d7d24f18539bcc053ab
Laurene Freyer <laurene.lepvrier@hotmail.fr>
2023-06-28 12:56:54 +0200

    datepicker

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,6 +3,6 @@ import flatpickr from "flatpickr"; // You need to import this to use new flatpic
 
 export default class extends Controller {
   connect() {
-    flatpickr(this.element)
+    flatpickr(this.element, { altInput: true, enableTime: true, altFormat: 'j F Y', minDate: 'today' })
   }
 }
