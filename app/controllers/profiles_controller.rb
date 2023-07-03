7fd2c5d1219714308f9ab862ddf19d21cfb0d236
Laurene Freyer <laurene.lepvrier@hotmail.fr>
2023-07-03 18:03:08 +0200

    ratings stars

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,12 @@
 class ProfilesController < ApplicationController
   def show
     @user = current_user
+    @ratings = Rating.where(user: @user)
+    if @ratings.size <= 1
+      @sum = 0
+    else
+      @sum = @ratings.pluck(:note).sum / @ratings.size
+    end
   end
 
   def autres
@@ -8,6 +14,12 @@ class ProfilesController < ApplicationController
     @rating = Rating.new
     if params[:params1]
       @activity = Activity.find(params[:params1])
+    end
+    @ratings = Rating.where(user: @user)
+    if @ratings.size <= 1
+      @sum = 0
+    else
+      @sum = @ratings.pluck(:note).sum / @ratings.size
     end
   end
 
